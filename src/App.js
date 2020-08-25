d2c9bb6d407a53dacac6a82e6379fb0dbaecfbd9
juan rios <juan.rios49@eia.edu.co>
2020-08-24 19:11:15 -0500

    a

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import logo from './assets/images/logo.svg';
 import './assets/css/App.css';
 import MiComponente from './components/MiComponente.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <section className="comonentes">
-          <MiComponente/>
-        </section>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <section className="comonentes">
+                        <MiComponente/>
+                    </section>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
