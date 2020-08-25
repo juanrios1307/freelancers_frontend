2ff89ed89246276adee54ab93449e301c76b03f0
JuanseHenao <juanse41601@gmail.com>
2020-08-25 09:31:51 -0500

    Update CSS2

---
+++ src/components/MainPage.js
@@ -0,0 +1,18 @@
+import React,{Component} from 'react';
+//import './src/assets/css/MainPage.css';
+
+class MainPage extends Component {
+    render(){
+        return (
+             <div className="navbar">
+                 <ul>
+                     <li><a href="#">Home</a></li>
+                     <li><a href="#">News</a></li>
+                     <li><a href="#">Contact</a></li>
+                     <li><a href="#">About</a></li>
+                 </ul>
+             </div>
+        );                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
+    }
+}
+export default MainPage;
