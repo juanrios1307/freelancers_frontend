d188b04581d061c47dc384eb2ddb096c694d2ac7
JuanseHenao <53921087+JuanseHenao@users.noreply.github.com>
2020-08-25 11:08:35 -0500

    Update Banner

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -3,12 +3,17 @@ import '../assets/css/MainPage.css';
 import NavBar from "./NavBar";
 import logo from '../assets/images/logo.svg'
 
+
 class MainPage extends Component {
     render(){
         return (
             <div>
                 <header className="App-header">
                     <NavBar/>
+                    <div className="banner">
+
+                    </div>
+
                 </header>
                 <img src={logo} className="align-self-center  mr-3" alt="banner"/>
 
