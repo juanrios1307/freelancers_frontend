66a4c7fcfeaec0d73c15b33f956e4cbf1bc72e9c
JuanseHenao <53921087+JuanseHenao@users.noreply.github.com>
2020-09-05 17:35:36 -0500

    Update New BannerSection

--- src/components/BannerSection.js
+++ src/components/BannerSection.js
@@ -1,11 +1,36 @@
-import React from "react";
+import React,{Component} from 'react';
+import '../assets/css/BannerSection.css'
 
-function BannerSection(){
-    return(
-        <div className='banner-container'>
-            
-        </div>
-    )
+class BannerSection extends Component {
+
+    render () {
+        return (
+            <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                        <img className="d-block w-100" src="../assets/images/slide1.jpg" alt="First slide"/>
+                    </div>
+                    <div className="carousel-item">
+                        <img className="d-block w-100" src="../assets/images/slide2.jpg" alt="Second slide"/>
+                    </div>
+                    <div className="carousel-item">
+                        <img className="d-block w-100" src="../assets/images/slide3.jpg" alt="Third slide"/>
+                    </div>
+                </div>
+                <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="sr-only">Previous</span>
+                </a>
+                <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="sr-only">Next</span>
+                </a>
+            </div>
+        )
+    }
 }
 
-export default BannerSection
+
+export default BannerSection;
+
+
--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -2,18 +2,25 @@ import React,{Component} from 'react';
 import '../assets/css/MainPage.css';
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Navbar2 from "./Navbar2";
+import BannerSection from "./BannerSection";
 
 class MainPage extends Component {
     render(){
         return (
-            <>
+            <body>
+            <header>
                 <Router>
                     <Navbar2/>
                     <Switch>
                         <Route path="/" exact />
                     </Switch>
                 </Router>
-            </>
+            </header>
+            <div>
+                <BannerSection/>
+            </div>
+
+            </body>
         );                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
     }
 }
